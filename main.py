214d5a6bc149a64c37c709b420b4274cd15be024
Charles Lewis <charlesslewis10@gmail.com>
2024-02-07 16:30:45 -0800

    new version

--- main.py
+++ main.py
@@ -22,37 +22,6 @@ def data():
         else:
             return render_template('404.html')
 
-
-        # Process the form data (e.g., save to a database)
-        # ...
-
-        # Display the form data on a separate page (data.html)
-        
-    
-
-# @app.route('/search', methods=['GET'])
-# def landing_search():
-#     # Process form data (e.g., validate, save to database, etc.)
-#     form_data = request.form
-#     query = form_data.get('eula')
-    
-#     # Determine the target page based on the form input
-#     if query == 'facebook':
-#         target_page = '/facebook'
-#     else:
-#         target_page = '/not_found'
-
-#     # Redirect to the appropriate page
-#     return redirect(target_page)
-
-# @app.route('/facebook')
-# def admin_dashboard():
-#     return "Welcome to the Admin Dashboard!"
-
-# @app.route('/not_found')
-# def user_dashboard():
-#     return "Welcome to the User Dashboard!"
-
 if __name__ == '__main__':
     app.run(debug=True)
 
